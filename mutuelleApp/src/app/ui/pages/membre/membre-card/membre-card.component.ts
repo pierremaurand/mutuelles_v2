2a0048494fe598a05c3b7deae7a8d927aeb2b6e8
Pierre Maurand OVASSA <pierremaurand@gmail.com>
2025-08-14 00:05:52 +0100

    modified:   mutuelleApp/src/app/ui/pages/membre/membre-card/membre-card.component.html 	modified:   mutuelleApp/src/app/ui/pages/membre/membre-card/membre-card.component.ts

--- mutuelleApp/src/app/ui/pages/membre/membre-card/membre-card.component.ts
+++ mutuelleApp/src/app/ui/pages/membre/membre-card/membre-card.component.ts
@@ -1,13 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Membre } from '../../../../core/models/membre';
-import { DecimalPipe, JsonPipe, UpperCasePipe } from '@angular/common';
+import {
+  DatePipe,
+  DecimalPipe,
+  JsonPipe,
+  UpperCasePipe,
+} from '@angular/common';
 import { environment } from '../../../../../environments/environment';
 import { Router } from '@angular/router';
 import { Sexe } from '../../../../core/models/sexe';
 
 @Component({
   selector: 'app-membre-card',
-  imports: [UpperCasePipe, DecimalPipe],
+  imports: [UpperCasePipe, DecimalPipe, DatePipe],
   templateUrl: './membre-card.component.html',
   styleUrl: './membre-card.component.scss',
 })
@@ -32,5 +37,13 @@ export class MembreCardComponent {
     return this.membre.sexe == Sexe.Feminin
       ? './assets/images/default_woman.jpg'
       : './assets/images/default_man.jpg';
+  }
+
+  get bgColor(): string {
+    return this.membre.sexe == Sexe.Feminin ? 'bg-danger' : 'bg-primary';
+  }
+
+  get dateStart(): string {
+    return this.membre.sexe == Sexe.Feminin ? 'Née le ' : 'Né le ';
   }
 }
